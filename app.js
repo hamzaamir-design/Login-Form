7c5a81f214eb06f75aea73eb1a0b03a68d933a4a
hamzaamir-design <hamzaamir9733@gmail.com>
2025-06-10 06:24:11 -0700

    Added html,css and Js files

---
+++ app.js
@@ -0,0 +1,4 @@
+let btn = document.querySelector('.form-btn'); // selects first element with class "form-btn"
+btn.onclick = () => {
+    alert("Congratulations! Your form is submitted successfully. ❤️💕");
+};
